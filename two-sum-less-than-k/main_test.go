f8a83b15dd4359b0da7bfbe8f50dfb3e9d95311d
xingxingso <24400338+xingxingso@users.noreply.github.com>
2021-05-16 13:15:13 +0800

    update

--- two-sum-less-than-k/main_test.go
+++ two-sum-less-than-k/main_test.go
@@ -13,7 +13,7 @@ func Test_twoSumLessThanK(t *testing.T) {
 		want int
 	}{
 		{
-			name: "equal",
+			name: "ex1",
 			args: args{
 				nums: []int{34, 23, 1, 24, 75, 33, 54, 8},
 				k:    60,
@@ -21,7 +21,7 @@ func Test_twoSumLessThanK(t *testing.T) {
 			want: 58,
 		},
 		{
-			name: "equal",
+			name: "ex2",
 			args: args{
 				nums: []int{10, 20, 30},
 				k:    15,
@@ -29,7 +29,7 @@ func Test_twoSumLessThanK(t *testing.T) {
 			want: -1,
 		},
 		{
-			name: "equal",
+			name: "ex3",
 			args: args{
 				nums: []int{254, 914, 110, 900, 147, 441, 209, 122, 571, 942, 136, 350, 160, 127, 178, 839, 201, 386, 462, 45, 735, 467, 153, 415, 875, 282, 204, 534, 639, 994, 284, 320, 865, 468, 1, 838, 275, 370, 295, 574, 309, 268, 415, 385, 786, 62, 359, 78, 854, 944},
 				k:    200,
