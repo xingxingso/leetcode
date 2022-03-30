da85c387d4b86c1d6e604bc7eee17f85f4e4ebba
尼采 <1126973789@qq.com>
2022-03-30 16:29:30 +0800

    update

--- course-schedule/main.go
+++ course-schedule/main.go
@@ -25,7 +25,7 @@ package course_schedule
 // --- 自己
 
 /*
-方法一:拓扑排序
+方法一:拓扑排序(广度优先搜索)
 
 时间复杂度：
 空间复杂度：
@@ -74,4 +74,50 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	}
 
 	return count == 0
+}
+
+// --- 官方
+
+/*
+方法一:拓扑排序(深度优先搜索)
+
+时间复杂度：
+空间复杂度：
+*/
+func canFinishO1(numCourses int, prerequisites [][]int) bool {
+	var (
+		edges   = make([][]int, numCourses)
+		visited = make([]int, numCourses)
+		result  []int
+		valid   = true
+		dfs     func(u int)
+	)
+
+	for _, info := range prerequisites {
+		edges[info[1]] = append(edges[info[1]], info[0])
+	}
+
+	dfs = func(u int) {
+		visited[u] = 1
+		for _, v := range edges[u] {
+			if visited[v] == 0 {
+				dfs(v)
+				if !valid {
+					return
+				}
+			} else if visited[v] == 1 {
+				valid = false
+				return
+			}
+		}
+		visited[u] = 2
+		result = append(result, u)
+	}
+
+	for i := 0; i < numCourses && valid; i++ {
+		if visited[i] == 0 {
+			dfs(i)
+		}
+	}
+	return valid
 }
